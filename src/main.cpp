2df1c900b9fac3c3b7f97eccb137e0a460d0a9f4
Avanish Tiwari <avanish.tiwari@thinci.com>
2021-07-13 02:33:35 +0530

    Add Cmake Build Files

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+# include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    std::cout<<"\n It WORKED!!!"<<std::endl;
+    return 0;
+}
---
+++ test/main.cpp
@@ -0,0 +1,7 @@
+# include<bits/stdc++.h>
+
+int main()
+{
+    std::cout<<"\n The test Bonary is working!!!"<<std::endl;
+    return 0;   
+}
